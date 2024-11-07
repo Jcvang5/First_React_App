af3e712556ae1d6618bf2bc52104cb0e8b3d0b02
Jason Vang <jvang@Jasons-Laptop.local>
2024-11-07 16:39:48 -0600

    Creating list with console.log onclick

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,22 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Paris", "Tokyo", "London"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
